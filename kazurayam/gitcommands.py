e867025e5cd7cb45bd36ec0b8b82db3a2492e213
kazurayam <kazuaki.urayama@gmail.com>
2021-06-08 22:21:28 +0900

    in the gitcommands.py, prefer completed_process to output as a variable name

--- kazurayam/gitcommands.py
+++ kazurayam/gitcommands.py
@@ -3,16 +3,16 @@ import subprocess
 from subprocess import PIPE, STDOUT, Popen
 
 
-def get_git_msg(output) -> str:
+def get_git_msg(completed_process) -> str:
     try:
-        msg = output.stdout.decode("ascii").strip()
+        msg = completed_process.stdout.decode("ascii").strip()
         return msg
     except UnicodeDecodeError:
-        return "!!!! binary file, unable to decode as ascii characters !!!!"
+        return "!!!! stdout is a binary sequence !!!!"
 
 
-def print_git_msg(output):
-    print("{}".format(get_git_msg(output)))
+def print_git_msg(completed_process):
+    print("{}".format(get_git_msg(completed_process)))
 
 
 def init(wt: str, verbose=True) -> str:
@@ -22,75 +22,75 @@ def init(wt: str, verbose=True) -> str:
     :param verbose:
     :return: STDOUT+STDERROR emitted by the process where the git command was executed
     """
-    output = subprocess.run('git init'.split(), cwd=wt, stdout=PIPE, stderr=STDOUT)
+    completed_process = subprocess.run('git init'.split(), cwd=wt, stdout=PIPE, stderr=STDOUT)
     if verbose:
         print("\n% git init")
-        print_git_msg(output)
-    return get_git_msg(output)
+        print_git_msg(completed_process)
+    return get_git_msg(completed_process)
 
 
 def add(wt, path, verbose=True) -> str:
-    output = subprocess.run(['git', 'add', path], cwd=wt, stdout=PIPE, stderr=STDOUT)
+    completed_process = subprocess.run(['git', 'add', path], cwd=wt, stdout=PIPE, stderr=STDOUT)
     if verbose:
         print("\n% git add", path)
-        print_git_msg(output)
-    return get_git_msg(output)
+        print_git_msg(completed_process)
+    return get_git_msg(completed_process)
 
 
 def status(wt, verbose=True) -> str:
-    output = subprocess.run(['git', 'status'], cwd=wt, stdout=PIPE, stderr=STDOUT)
+    completed_process = subprocess.run(['git', 'status'], cwd=wt, stdout=PIPE, stderr=STDOUT)
     if verbose:
         print("\n% git status")
-        print_git_msg(output)
-    return get_git_msg(output)
+        print_git_msg(completed_process)
+    return get_git_msg(completed_process)
 
 
 def commit(wt, msg, verbose=True):
-    output = subprocess.run(['git', 'commit', '-m', msg], cwd=wt, stdout=PIPE, stderr=STDOUT)
+    completed_process = subprocess.run(['git', 'commit', '-m', msg], cwd=wt, stdout=PIPE, stderr=STDOUT)
     if verbose:
         print("\n% git commit -m \"{}\"".format(msg))
-        print_git_msg(output)
-    return get_git_msg(output)
+        print_git_msg(completed_process)
+    return get_git_msg(completed_process)
 
 
 def catfile_t(wt, gitobject: str, verbose=True) -> str:
-    output = subprocess.run(['git', 'cat-file', '-t', gitobject], cwd=wt, stdout=PIPE, stderr=STDOUT)
+    completed_process = subprocess.run(['git', 'cat-file', '-t', gitobject], cwd=wt, stdout=PIPE, stderr=STDOUT)
     if verbose:
         print("\n% git cat-file -t", gitobject)
-        print_git_msg(output)
-    return get_git_msg(output)
+        print_git_msg(completed_process)
+    return get_git_msg(completed_process)
 
 
 def catfile_p(wt, gitobject: str, verbose=True) -> str:
-    output = subprocess.run(['git', 'cat-file', '-p', gitobject], cwd=wt, stdout=PIPE, stderr=STDOUT)
+    completed_process = subprocess.run(['git', 'cat-file', '-p', gitobject], cwd=wt, stdout=PIPE, stderr=STDOUT)
     if verbose:
         print("\n% git cat-file -p", gitobject)
-        print_git_msg(output)
-    return get_git_msg(output)
+        print_git_msg(completed_process)
+    return get_git_msg(completed_process)
 
 
 def catfile_blob(wt, gitobject: str, verbose=True) -> str:
-    output = subprocess.run(['git', 'cat-file', 'blob', gitobject], cwd=wt, stdout=PIPE, stderr=STDOUT)
+    completed_process = subprocess.run(['git', 'cat-file', 'blob', gitobject], cwd=wt, stdout=PIPE, stderr=STDOUT)
     if verbose:
         print("\n% git cat-file blob", gitobject)
-        print_git_msg(output)
-    return get_git_msg(output)
+        print_git_msg(completed_process)
+    return get_git_msg(completed_process)
 
 
 def revparse(wt, gitobject: str, verbose=True) -> str:
-    output = subprocess.run(['git', 'rev-parse', gitobject], cwd=wt, stdout=PIPE, stderr=STDOUT)
+    completed_process = subprocess.run(['git', 'rev-parse', gitobject], cwd=wt, stdout=PIPE, stderr=STDOUT)
     if verbose:
         print("\n% git rev-parse", gitobject)
-        print_git_msg(output)
-    return get_git_msg(output)
+        print_git_msg(completed_process)
+    return get_git_msg(completed_process)
 
 
 def lstree(wt, gitobject: str, verbose=True) -> str:
-    output = subprocess.run(['git', 'ls-tree', gitobject], cwd=wt, stdout=PIPE, stderr=STDOUT)
+    completed_process = subprocess.run(['git', 'ls-tree', gitobject], cwd=wt, stdout=PIPE, stderr=STDOUT)
     if verbose:
         print("\n% git ls-tree", gitobject)
-        print_git_msg(output)
-    return get_git_msg(output)
+        print_git_msg(completed_process)
+    return get_git_msg(completed_process)
 
 
 def lsfiles_stage(wt, verbose=True) -> str:
@@ -101,11 +101,11 @@ def lsfiles_stage(wt, verbose=True) -> str:
     :param verbose:
     :return:
     """
-    output = subprocess.run(['git', 'ls-files', '--stage'], cwd=wt, stdout=PIPE, stderr=STDOUT)
+    completed_process = subprocess.run(['git', 'ls-files', '--stage'], cwd=wt, stdout=PIPE, stderr=STDOUT)
     if verbose:
         print("\n% git ls-files --stage")
-        print_git_msg(output)
-    return get_git_msg(output)
+        print_git_msg(completed_process)
+    return get_git_msg(completed_process)
 
 
 def showref_heads(wt, verbose=True) -> str:
@@ -120,11 +120,11 @@ b114566da8f14ed186efba10388d47979c78e4f5 refs/heads/master
     :param verbose:
     :return:
     """
-    output = subprocess.run(['git', 'show-ref', '--heads'], cwd=wt, stdout=PIPE, stderr=STDOUT)
+    completed_process = subprocess.run(['git', 'show-ref', '--heads'], cwd=wt, stdout=PIPE, stderr=STDOUT)
     if verbose:
         print("\n% git show-ref --heads")
-        print_git_msg(output)
-    return get_git_msg(output)
+        print_git_msg(completed_process)
+    return get_git_msg(completed_process)
 
 
 def branch_new(wt, branch_name: str, verbose=True) -> str:
@@ -135,11 +135,12 @@ def branch_new(wt, branch_name: str, verbose=True) -> str:
     :param verbose:
     :return:
     """
-    output = subprocess.run(['git', 'branch', branch_name], cwd=wt, stdout=PIPE, stderr=STDOUT)
+    completed_process = subprocess.run(['git', 'branch', branch_name], cwd=wt, stdout=PIPE, stderr=STDOUT)
     if verbose:
         print("\n% git branch", branch_name)
-        print_git_msg(output)
-    return get_git_msg(output)
+        print_git_msg(completed_process)
+    return get_git_msg(completed_process)
+
 
 def branch_show_current(wt, verbose=True) -> str:
     """
@@ -149,11 +150,11 @@ def branch_show_current(wt, verbose=True) -> str:
     :param verbose:
     :return:
     """
-    output = subprocess.run(['git', 'branch', '--show-current'], cwd=wt, stdout=PIPE, stderr=STDOUT)
+    completed_process = subprocess.run(['git', 'branch', '--show-current'], cwd=wt, stdout=PIPE, stderr=STDOUT)
     if verbose:
         print("\n% git branch", "--show-current")
-        print_git_msg(output)
-    return get_git_msg(output)
+        print_git_msg(completed_process)
+    return get_git_msg(completed_process)
 
 
 def checkout(wt, branch_name: str, verbose=True) -> str:
@@ -164,11 +165,11 @@ def checkout(wt, branch_name: str, verbose=True) -> str:
     :param verbose:
     :return:
     """
-    output = subprocess.run(['git', 'checkout', branch_name], cwd=wt, stdout=PIPE, stderr=STDOUT)
+    completed_process = subprocess.run(['git', 'checkout', branch_name], cwd=wt, stdout=PIPE, stderr=STDOUT)
     if verbose:
         print("\n% git checkout", branch_name)
-        print_git_msg(output)
-    return get_git_msg(output)
+        print_git_msg(completed_process)
+    return get_git_msg(completed_process)
 
 
 def merge(wt, branch_name: str, verbose=True) -> str:
@@ -179,11 +180,11 @@ def merge(wt, branch_name: str, verbose=True) -> str:
     :param verbose:
     :return:
     """
-    output = subprocess.run(['git', 'merge', branch_name], cwd=wt, stdout=PIPE, stderr=STDOUT)
+    completed_process = subprocess.run(['git', 'merge', branch_name], cwd=wt, stdout=PIPE, stderr=STDOUT)
     if verbose:
         print("\n% git merge", branch_name)
-        print_git_msg(output)
-    return get_git_msg(output)
+        print_git_msg(completed_process)
+    return get_git_msg(completed_process)
 
 
 def tag_to(wt, tag_name: str, refer_to: str = 'HEAD', verbose=True) -> str:
@@ -195,11 +196,11 @@ def tag_to(wt, tag_name: str, refer_to: str = 'HEAD', verbose=True) -> str:
     :param verbose:
     :return:
     """
-    output = subprocess.run(['git', 'tag', tag_name, refer_to], cwd=wt, stdout=PIPE, stderr=STDOUT)
+    completed_process = subprocess.run(['git', 'tag', tag_name, refer_to], cwd=wt, stdout=PIPE, stderr=STDOUT)
     if verbose:
         print("\n% git tag", tag_name, refer_to)
-        print_git_msg(output)
-    return get_git_msg(output)
+        print_git_msg(completed_process)
+    return get_git_msg(completed_process)
 
 
 def tag_points_at(wt, object: str, verbose=True) -> tuple:
@@ -214,10 +215,10 @@ def tag_points_at(wt, object: str, verbose=True) -> tuple:
     :param verbose:
     :return: a tuple of (stdout, return_code)
     """
-    output = subprocess.run(['git', 'tag', '--points-at', object],
+    completed_process = subprocess.run(['git', 'tag', '--points-at', object],
                             cwd=wt, stdout=PIPE, stderr=STDOUT,
                             check=False)
     if verbose:
         print("\n% git tag --points-at", object)
-        print_git_msg(output)
-    return get_git_msg(output), output.returncode
+        print_git_msg(completed_process)
+    return get_git_msg(completed_process), completed_process.returncode
