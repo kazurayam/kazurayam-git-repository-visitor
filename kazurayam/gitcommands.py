f8d3c41c2747baf3b34dc82124b3bf877e4468e0
kazurayam <kazuaki.urayama@gmail.com>
2021-06-13 13:11:56 +0900

    sorted functions by name in gitcommands.py and gitcommands_test.py

--- kazurayam/gitcommands.py
+++ kazurayam/gitcommands.py
@@ -4,89 +4,8 @@ from .shellcommand import decode_stdout, print_msg
 from .shellcommand import shell_command
 
 
-def init(wt: str, verbose=False) -> str:
-    # cp stands for Completed Process as defined in Python subprocess
-    args = ['git', 'init']
-    cp = shell_command(wt, args, verbose=verbose)
-    return cp.stdout
-
-
 def add(wt, path, verbose=False) -> str:
     args = ["git", "add", path]
-    cp = shell_command(wt, args, verbose=verbose)
-    return cp.stdout
-
-
-def status(wt, verbose=False) -> str:
-    args = ['git', 'status']
-    cp = shell_command(wt, args, verbose=verbose)
-    return cp.stdout
-
-
-def commit(wt, msg, verbose=False) -> str:
-    args = ['git', 'commit', '-m', msg]
-    cp = shell_command(wt, args, verbose=verbose)
-    return cp.stdout
-
-
-def catfile_t(wt, gitobject: str, verbose=False) -> str:
-    args = ['git', 'cat-file', '-t', gitobject]
-    cp = shell_command(wt, args, verbose=verbose)
-    return cp.stdout
-
-
-def catfile_p(wt, gitobject: str, verbose=False) -> str:
-    args = ['git', 'cat-file', '-p', gitobject]
-    cp = shell_command(wt, args, verbose=verbose)
-    return cp.stdout
-
-
-def catfile_blob(wt, gitobject: str, verbose=False) -> str:
-    completed_process = subprocess.run(['git', 'cat-file', 'blob', gitobject], cwd=wt, stdout=PIPE, stderr=STDOUT)
-    if verbose:
-        print("\n% git cat-file blob", gitobject)
-        print_msg(completed_process)
-    return decode_stdout(completed_process)
-
-
-def revparse(wt, gitobject: str, verbose=False) -> subprocess.CompletedProcess:
-    args = ['git', 'rev-parse', gitobject]
-    cp = shell_command(wt, args, verbose=verbose)
-    return cp
-
-
-def lstree(wt, gitobject: str, verbose=False) -> str:
-    args = ['git', 'ls-tree', gitobject]
-    cp = shell_command(wt, args, verbose=verbose)
-    return cp.stdout
-
-
-def lsfiles_stage(wt, verbose=False) -> str:
-    """
-    execute `git ls-file --stage` command
-    this command shows the content of the Index
-    :param wt:
-    :param verbose:
-    :return:
-    """
-    args = ['git', 'ls-files', '--stage']
-    cp = shell_command(wt, args, verbose=verbose)
-    return cp.stdout
-
-
-def showref_heads(wt, verbose=False) -> str:
-    """
-    execute `git show-ref --heads` command
-    this command shows something like this:
-    ```
-    b114566da8f14ed186efba10388d47979c78e4f5 refs/heads/develop
-    b114566da8f14ed186efba10388d47979c78e4f5 refs/heads/master
-    ```
-    :param wt:
-    :param verbose:
-    :return:
-    """
-    args = ['git', 'show-ref', '--heads']
     cp = shell_command(wt, args, verbose=verbose)
     return cp.stdout
 
@@ -101,42 +20,6 @@ def branch_show_current(wt, verbose=False) -> str:
     args = ['git', 'branch', '--show-current']
     cp = shell_command(wt, args, verbose=verbose)
     return cp.stdout
-
-
-def checkout(wt, branch_name: str, verbose=False) -> str:
-    args = ['git', 'checkout', branch_name]
-    cp = shell_command(wt, args, verbose=verbose)
-    return cp.stdout
-
-
-def merge(wt, branch_name: str, verbose=False) -> str:
-    args = ['git', 'merge', branch_name]
-    cp = shell_command(wt, args, verbose=verbose)
-    return cp.stdout
-
-
-def tag_to(wt, tag_name: str, refer_to: str = 'HEAD', verbose=False) -> str:
-    args = ['git', 'tag', tag_name, refer_to]
-    cp = shell_command(wt, args, verbose=verbose)
-    return cp.stdout
-
-
-def tag_points_at(wt, object: str, verbose=False) -> subprocess.CompletedProcess:
-    """
-    execute `git tag --points-at <object>` command.
-    retrieves tag_name that refers to the object.
-    If the tag was found, will return an instance of subprocess.CompletedProcess
-    with the returncode == 0 and the stdout (=tag name).
-    If the tag was NOT found, will return an instance of subprocess.CompletedProcess
-    with the returncode != 0. You should not refer to the stdout.
-    :param wt:
-    :param object:
-    :param verbose:
-    :return: subprocess.CompletedProcess
-    """
-    args = ['git', 'tag', '--points-at', object]
-    cp = shell_command(wt, args, verbose=verbose)
-    return cp
 
 
 def catfile_batchcheck_batchallobjects(wt:str, verbose=False) -> subprocess.CompletedProcess:
@@ -159,3 +42,120 @@ b371df9d9194821c4a54f0e3a77f89bbcee62f7e blob 25
     """
     args = ['git', 'cat-file', '--batch-check', '--batch-all-objects']
     return shell_command(wt, args, verbose)
+
+
+def catfile_blob(wt, gitobject: str, verbose=False) -> str:
+    completed_process = subprocess.run(['git', 'cat-file', 'blob', gitobject], cwd=wt, stdout=PIPE, stderr=STDOUT)
+    if verbose:
+        print("\n% git cat-file blob", gitobject)
+        print_msg(completed_process)
+    return decode_stdout(completed_process)
+
+
+def catfile_p(wt, gitobject: str, verbose=False) -> str:
+    args = ['git', 'cat-file', '-p', gitobject]
+    cp = shell_command(wt, args, verbose=verbose)
+    return cp.stdout
+
+
+def catfile_t(wt, gitobject: str, verbose=False) -> str:
+    args = ['git', 'cat-file', '-t', gitobject]
+    cp = shell_command(wt, args, verbose=verbose)
+    return cp.stdout
+
+
+def checkout(wt, branch_name: str, verbose=False) -> str:
+    args = ['git', 'checkout', branch_name]
+    cp = shell_command(wt, args, verbose=verbose)
+    return cp.stdout
+
+
+def commit(wt, msg, verbose=False) -> str:
+    args = ['git', 'commit', '-m', msg]
+    cp = shell_command(wt, args, verbose=verbose)
+    return cp.stdout
+
+
+def init(wt: str, verbose=False) -> str:
+    # cp stands for Completed Process as defined in Python subprocess
+    args = ['git', 'init']
+    cp = shell_command(wt, args, verbose=verbose)
+    return cp.stdout
+
+
+def lsfiles_stage(wt, verbose=False) -> str:
+    """
+    execute `git ls-file --stage` command
+    this command shows the content of the Index
+    :param wt:
+    :param verbose:
+    :return:
+    """
+    args = ['git', 'ls-files', '--stage']
+    cp = shell_command(wt, args, verbose=verbose)
+    return cp.stdout
+
+
+def lstree(wt, gitobject: str, verbose=False) -> str:
+    args = ['git', 'ls-tree', gitobject]
+    cp = shell_command(wt, args, verbose=verbose)
+    return cp.stdout
+
+
+def merge(wt, branch_name: str, verbose=False) -> str:
+    args = ['git', 'merge', branch_name]
+    cp = shell_command(wt, args, verbose=verbose)
+    return cp.stdout
+
+
+def revparse(wt, gitobject: str, verbose=False) -> subprocess.CompletedProcess:
+    args = ['git', 'rev-parse', gitobject]
+    cp = shell_command(wt, args, verbose=verbose)
+    return cp
+
+
+def showref_heads(wt, verbose=False) -> str:
+    """
+    execute `git show-ref --heads` command
+    this command shows something like this:
+    ```
+    b114566da8f14ed186efba10388d47979c78e4f5 refs/heads/develop
+    b114566da8f14ed186efba10388d47979c78e4f5 refs/heads/master
+    ```
+    :param wt:
+    :param verbose:
+    :return:
+    """
+    args = ['git', 'show-ref', '--heads']
+    cp = shell_command(wt, args, verbose=verbose)
+    return cp.stdout
+
+
+def status(wt, verbose=False) -> str:
+    args = ['git', 'status']
+    cp = shell_command(wt, args, verbose=verbose)
+    return cp.stdout
+
+
+def tag_points_at(wt, object: str, verbose=False) -> subprocess.CompletedProcess:
+    """
+    execute `git tag --points-at <object>` command.
+    retrieves tag_name that refers to the object.
+    If the tag was found, will return an instance of subprocess.CompletedProcess
+    with the returncode == 0 and the stdout (=tag name).
+    If the tag was NOT found, will return an instance of subprocess.CompletedProcess
+    with the returncode != 0. You should not refer to the stdout.
+    :param wt:
+    :param object:
+    :param verbose:
+    :return: subprocess.CompletedProcess
+    """
+    args = ['git', 'tag', '--points-at', object]
+    cp = shell_command(wt, args, verbose=verbose)
+    return cp
+
+
+def tag_to(wt, tag_name: str, refer_to: str = 'HEAD', verbose=False) -> str:
+    args = ['git', 'tag', tag_name, refer_to]
+    cp = shell_command(wt, args, verbose=verbose)
+    return cp.stdout
--- kazurayam/gitcommands_test.py
+++ kazurayam/gitcommands_test.py
@@ -19,117 +19,12 @@ def wt_with_initial_commit(basedir):
     yield wt
 
 
-def test_init(basedir):
-    (wt, gr) = testutils.create_subject_dir(basedir, 'test_init')
-    stdout = GIT.init(wt)
-    assert stdout.startswith("hint:")
-
-
 def test_add(basedir):
     (wt, gr) = testutils.create_subject_dir(basedir, 'test_add')
     fileutils.write_file(wt, '.gitignore', '*~\n')
     stdout = GIT.init(wt)
     stdout = GIT.add(wt, '.')
     assert stdout == ""
-
-
-def test_commit(basedir):
-    (wt, gr) = testutils.create_subject_dir(basedir, 'test_commit')
-    fileutils.write_file(wt, '.gitignore', '*~\n')
-    stdout = GIT.init(wt)
-    stdout = GIT.add(wt, '.')
-    stdout = GIT.commit(wt, 'add .gitignore')
-    assert '.gitignore' in stdout
-
-
-def test_status(wt_with_initial_commit):
-    stdout = GIT.status(wt_with_initial_commit)
-    assert stdout.startswith("On branch master")
-
-
-def test_catfile_t(wt_with_initial_commit):
-    stdout = GIT.catfile_t(wt_with_initial_commit, 'HEAD')
-    """
-    $ git cat-file -t HEAD
-    commit
-    """
-    assert stdout == 'commit'
-
-
-def test_catfile_p(wt_with_initial_commit):
-    stdout = GIT.catfile_p(wt_with_initial_commit, 'HEAD')
-    """
-    $ git cat-file -p HEAD
-    tree ...
-    author ...
-    committer ...
-    
-    message
-    """
-    assert stdout.startswith('tree')
-
-
-def test_catfile_blob(wt_with_initial_commit):
-    cp = shell_command(wt_with_initial_commit, ['git', 'ls-tree', '-r', 'HEAD'])
-    """
-    $ git ls-tree -r HEAD
-    100644 blob fb7c93af634239c075b1b0c8c6c19bbeede0ed54    .DS_Store
-    100644 blob 3e152b50e7ea36b543d598cf626fcd631cdc6c49    .gitignore
-    ...
-    """
-    blob_hash = None
-    for line in cp.stdout.splitlines():
-        if line.split()[3] == '.gitignore':
-            blob_hash = line.split()[2]
-    assert blob_hash is not None
-    stdout = GIT.catfile_p(wt_with_initial_commit, blob_hash)
-    assert stdout.startswith('*~')
-
-
-def test_revparse(wt_with_initial_commit):
-    # cp stands for subprocess.CompletedProcess
-    cp = GIT.revparse(wt_with_initial_commit, 'HEAD')
-    """
-    $ git rev-parse HEAD
-    1e29db36da84624f599357777d986fa37aa5869d
-    """
-    assert re.match(r'^[0-9a-f]{40}', cp.stdout)
-
-
-def test_lstree(wt_with_initial_commit):
-    stdout = GIT.lstree(wt_with_initial_commit, 'HEAD')
-    """
-    $ git ls-tree HEAD
-100644 blob fb7c93af634239c075b1b0c8c6c19bbeede0ed54    .DS_Store
-100644 blob 3e152b50e7ea36b543d598cf626fcd631cdc6c49    .gitignore
-...
-    """
-    assert len(stdout.splitlines()) > 0
-    assert re.match(r'^[0-9a-f]{40}', stdout.splitlines()[0].split()[2])
-
-
-def test_lsfiles_stage(wt_with_initial_commit):
-    stdout = GIT.lsfiles_stage(wt_with_initial_commit)
-    """
-    $ git ls-files --stage
-100644 fb7c93af634239c075b1b0c8c6c19bbeede0ed54 0       .DS_Store
-100644 3e152b50e7ea36b543d598cf626fcd631cdc6c49 0       .gitignore
-...
-    """
-    assert len(stdout.splitlines()) > 0
-    assert re.match(r'^[0-9a-f]{40}', stdout.splitlines()[0].split()[1])
-
-
-def test_showref_heads(wt_with_initial_commit):
-    stdout = GIT.showref_heads(wt_with_initial_commit, verbose=False)
-    """
-    $ git show-ref --heads
-    b114566da8f14ed186efba10388d47979c78e4f5 refs/heads/develop
-    b114566da8f14ed186efba10388d47979c78e4f5 refs/heads/master
-    """
-    assert len(stdout.splitlines()) > 0
-    assert re.match(r'^[0-9a-f]{40}', stdout.splitlines()[0].split()[0])
-    assert 'refs/heads/master' in stdout
 
 
 def test_branch_new(wt_with_initial_commit):
@@ -149,6 +44,62 @@ def test_branch_new(wt_with_initial_commit):
     assert 'master' in cp.stdout
 
 
+def test_branch_show_current(wt_with_initial_commit):
+    o = GIT.branch_show_current(wt_with_initial_commit)
+    """
+    $ git branch --show--current
+    master
+    """
+    assert "master" in o
+
+
+def test_catfile_batchcheck_batchallobjects(wt_with_initial_commit):
+    completed_process = GIT.catfile_batchcheck_batchallobjects(wt_with_initial_commit)
+    assert completed_process.returncode == 0
+    lines = completed_process.stdout.splitlines()
+    assert len(lines) == 6
+    # 1 commit, 2 trees, 3 blobs = 6 objects
+
+
+def test_catfile_blob(wt_with_initial_commit):
+    cp = shell_command(wt_with_initial_commit, ['git', 'ls-tree', '-r', 'HEAD'])
+    """
+    $ git ls-tree -r HEAD
+    100644 blob fb7c93af634239c075b1b0c8c6c19bbeede0ed54    .DS_Store
+    100644 blob 3e152b50e7ea36b543d598cf626fcd631cdc6c49    .gitignore
+    ...
+    """
+    blob_hash = None
+    for line in cp.stdout.splitlines():
+        if line.split()[3] == '.gitignore':
+            blob_hash = line.split()[2]
+    assert blob_hash is not None
+    stdout = GIT.catfile_p(wt_with_initial_commit, blob_hash)
+    assert stdout.startswith('*~')
+
+
+def test_catfile_p(wt_with_initial_commit):
+    stdout = GIT.catfile_p(wt_with_initial_commit, 'HEAD')
+    """
+    $ git cat-file -p HEAD
+    tree ...
+    author ...
+    committer ...
+    
+    message
+    """
+    assert stdout.startswith('tree')
+
+
+def test_catfile_t(wt_with_initial_commit):
+    stdout = GIT.catfile_t(wt_with_initial_commit, 'HEAD')
+    """
+    $ git cat-file -t HEAD
+    commit
+    """
+    assert stdout == 'commit'
+
+
 def test_checkout(wt_with_initial_commit):
     o = GIT.branch_new(wt_with_initial_commit, "develop")
     """
@@ -163,13 +114,43 @@ def test_checkout(wt_with_initial_commit):
     assert "Switched to branch 'develop'" in o
 
 
-def test_branch_show_current(wt_with_initial_commit):
-    o = GIT.branch_show_current(wt_with_initial_commit)
+def test_commit(basedir):
+    (wt, gr) = testutils.create_subject_dir(basedir, 'test_commit')
+    fileutils.write_file(wt, '.gitignore', '*~\n')
+    stdout = GIT.init(wt)
+    stdout = GIT.add(wt, '.')
+    stdout = GIT.commit(wt, 'add .gitignore')
+    assert '.gitignore' in stdout
+
+
+def test_init(basedir):
+    (wt, gr) = testutils.create_subject_dir(basedir, 'test_init')
+    stdout = GIT.init(wt)
+    assert stdout.startswith("hint:")
+
+
+def test_lsfiles_stage(wt_with_initial_commit):
+    stdout = GIT.lsfiles_stage(wt_with_initial_commit)
     """
-    $ git branch --show--current
-    master
+    $ git ls-files --stage
+    100644 fb7c93af634239c075b1b0c8c6c19bbeede0ed54 0       .DS_Store
+    100644 3e152b50e7ea36b543d598cf626fcd631cdc6c49 0       .gitignore
+    ...
     """
-    assert "master" in o
+    assert len(stdout.splitlines()) > 0
+    assert re.match(r'^[0-9a-f]{40}', stdout.splitlines()[0].split()[1])
+
+
+def test_lstree(wt_with_initial_commit):
+    stdout = GIT.lstree(wt_with_initial_commit, 'HEAD')
+    """
+    $ git ls-tree HEAD
+100644 blob fb7c93af634239c075b1b0c8c6c19bbeede0ed54    .DS_Store
+100644 blob 3e152b50e7ea36b543d598cf626fcd631cdc6c49    .gitignore
+...
+    """
+    assert len(stdout.splitlines()) > 0
+    assert re.match(r'^[0-9a-f]{40}', stdout.splitlines()[0].split()[2])
 
 
 def test_merge(wt_with_initial_commit):
@@ -194,11 +175,31 @@ def test_merge(wt_with_initial_commit):
     assert content in GIT.catfile_p(wt_with_initial_commit, blob_hash)
 
 
-def test_tag_to(wt_with_initial_commit):
-    o = GIT.tag_to(wt_with_initial_commit, '0.1.0')
-    cp = shell_command(wt_with_initial_commit, ['git', 'tag'])
-    assert len(cp.stdout.splitlines()) == 1
-    assert cp.stdout.splitlines()[0] == '0.1.0'
+def test_revparse(wt_with_initial_commit):
+    # cp stands for subprocess.CompletedProcess
+    cp = GIT.revparse(wt_with_initial_commit, 'HEAD')
+    """
+    $ git rev-parse HEAD
+    1e29db36da84624f599357777d986fa37aa5869d
+    """
+    assert re.match(r'^[0-9a-f]{40}', cp.stdout)
+
+
+def test_showref_heads(wt_with_initial_commit):
+    stdout = GIT.showref_heads(wt_with_initial_commit, verbose=False)
+    """
+    $ git show-ref --heads
+    b114566da8f14ed186efba10388d47979c78e4f5 refs/heads/develop
+    b114566da8f14ed186efba10388d47979c78e4f5 refs/heads/master
+    """
+    assert len(stdout.splitlines()) > 0
+    assert re.match(r'^[0-9a-f]{40}', stdout.splitlines()[0].split()[0])
+    assert 'refs/heads/master' in stdout
+
+
+def test_status(wt_with_initial_commit):
+    stdout = GIT.status(wt_with_initial_commit)
+    assert stdout.startswith("On branch master")
 
 
 def test_tag_points_at(wt_with_initial_commit):
@@ -208,9 +209,8 @@ def test_tag_points_at(wt_with_initial_commit):
     assert cp.stdout == '0.1.0'
 
 
-def test_catfile_batchcheck_batchallobjects(wt_with_initial_commit):
-    completed_process = GIT.catfile_batchcheck_batchallobjects(wt_with_initial_commit)
-    assert completed_process.returncode == 0
-    lines = completed_process.stdout.splitlines()
-    assert len(lines) == 6
-    # 1 commit, 2 trees, 3 blobs = 6 objects
+def test_tag_to(wt_with_initial_commit):
+    o = GIT.tag_to(wt_with_initial_commit, '0.1.0')
+    cp = shell_command(wt_with_initial_commit, ['git', 'tag'])
+    assert len(cp.stdout.splitlines()) == 1
+    assert cp.stdout.splitlines()[0] == '0.1.0'
